5d8caf50e435c199b3382193c88696076fb251e5
yfdo <dong.yufei@outlook.com>
2022-03-04 23:43:25 -0500

    part2 finished

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,7 +1,11 @@
 package com.example.flixster
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
+@Parcelize
 data class Movie(
     //represents one movie
     val movieId: Int,
@@ -9,10 +13,13 @@ data class Movie(
     val title: String,
     val overview: String,
     val backdropPath: String,
-    val popularity: Int
-){
+    val rating: Double
+) : Parcelable {
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+
     val posterLandImageUrl = "https://image.tmdb.org/t/p/w342/$backdropPath"
+
     //pass Json file to Kotlin class
     companion object {
         //allows us to call methods of the Movie class without having an instance
@@ -20,7 +27,7 @@ data class Movie(
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
             //iterate through the JSONarray to return a list of Movie
             val movies = mutableListOf<Movie>()
-            for(i in 0 until movieJsonArray.length()){
+            for (i in 0 until movieJsonArray.length()) {
                 val movieJson = movieJsonArray.getJSONObject(i)
                 movies.add(
                     Movie(
@@ -29,7 +36,7 @@ data class Movie(
                         movieJson.getString("title"),
                         movieJson.getString("overview"),
                         movieJson.getString("backdrop_path"),
-                        movieJson.getInt("vote_average")
+                        movieJson.getDouble("vote_average")
                     )
                 )
             }
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,14 +1,24 @@
 package com.example.flixster
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityOptionsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
+import androidx.core.util.Pair
+
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
+private const val TAG = "MovieAdapter"
 
 class MovieAdapter(private val context: Context, private val movies: List<Movie>) :
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -16,7 +26,7 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
     private val POPULAR = 1
 
     override fun getItemViewType(position: Int): Int {
-        if (movies[position].popularity > 5) {
+        if (movies[position].rating > 5) {
             //popular
             return POPULAR
         } else {
@@ -51,10 +61,15 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size //inline one-line function
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
+
+        init {
+            itemView.setOnClickListener(this)//pass in the class
+        }
 
         fun bind(movie: Movie) {
             //populate corresponding parts of itemView with data in movie
@@ -63,28 +78,43 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
             //check screen orientation
             val ori = context.resources.configuration.orientation
+            //rounded corners config
+            val radius = 50
+            val margin = 10
+
+
             if (ori == Configuration.ORIENTATION_PORTRAIT) {
                 Glide.with(context)
                     .load(movie.posterImageUrl)
                     .placeholder(R.drawable.ic_launcher_background)
+                    .centerCrop()
+                    .transform(RoundedCornersTransformation(radius, margin))
                     .into(ivPoster)
             } else {
                 Glide.with(context)
                     .load(movie.posterLandImageUrl)
                     .placeholder(R.drawable.ic_launcher_background)
+                    .centerCrop()
+                    .transform(RoundedCornersTransformation(radius, margin))
                     .into(ivPoster)
             }
         }
-    }
 
-//    inner class ViewHolderPopular(itemView: View) : RecyclerView.ViewHolder(itemView){
-//        private val ivBackdrop = itemView.findViewById<ImageView>(R.id.ivBackdrop)
-//
-//        fun bind(movie: Movie){
-//            Glide.with(context)
-//                .load(movie.posterLandImageUrl)
-//                .placeholder(R.drawable.ic_launcher_background)
-//                .into(ivBackdrop)
-//        }
-//    }
+        override fun onClick(p0: View?) {
+            // 1. Get notified of the particular movie which was clicked on
+            val movie = movies[adapterPosition]
+            // 2. Use intent to navigate to the new activity(screen)
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                context as Activity,
+                Pair<View, String>(tvTitle, "tvTitle-transition"),
+                Pair<View, String>(
+                    tvOverview, "tvOverview-transition"
+                )
+            )
+            context.startActivity(intent, options.toBundle());
+        }
+    }
 }
